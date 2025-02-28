d1c13b91b70a98d6229a3324bb675381ba26c581
suhanyujie <suhanyujie@qq.com>
2025-02-28 14:17:13 +0900

    update background color for link

--- resources/views/frontend/widgets/links.blade.php
+++ resources/views/frontend/widgets/links.blade.php
@@ -16,3 +16,40 @@
         <p class="link-gallery-empty">リンクはありません</p>
     <?php endif; ?>
 </div>
+
+<style>
+.link-gallery-list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+.link-gallery-item {
+    margin-bottom: 10px;
+}
+
+.link-gallery-item a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    background-color: #c0e4e4;
+    padding: 10px;
+    border-radius: 5px;
+    transition: background-color 0.2s;
+}
+
+.link-gallery-item a:hover {
+    background-color: #daf0f0;
+}
+
+.link-gallery-image {
+    width: 24px;
+    height: 24px;
+    margin-right: 10px;
+    border-radius: 3px;
+}
+
+.link-gallery-name {
+    color: #333;
+}
+</style>
