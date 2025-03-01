430511ded006c50a67bcd8db561b7effecda2ab3
suhanyujie <suhanyujie@qq.com>
2025-03-02 01:55:22 +0900

    update comment

--- link-gallery.php
+++ link-gallery.php
@@ -27,24 +27,9 @@ require_once LINK_GALLERY_PLUGIN_DIR . 'vendor/autoload.php';
 function link_gallery_init() {
     // 初始化数据库
     \LinkGallery\Database\Migration::createLinkTable();
-
-    // 添加后台菜单
-    // add_action('admin_menu', function() {
-    //     add_menu_page(
-    //         '友情链接管理', // 页面标题
-    //         '友情链接', // 菜单标题
-    //         'manage_options', // 权限
-    //         'link-gallery', // 菜单slug
-    //         function() {
-    //             // 后台页面内容将在这里添加
-    //             echo '<div class="wrap"><h1>友情链接管理</h1></div>';
-    //         },
-    //         'dashicons-admin-links', // 菜单图标
-    //         30 // 菜单位置
-    //     );
-    // });
-
+    // friend link 后台的菜单和页面显示
     new \LinkGallery\Controllers\Admin\LinkController();
+    // 友情链接相关的 gutenberg 编辑器组件
     new \LinkGallery\Controllers\Frontend\LinkWidgetController();
 }
 
