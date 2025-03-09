67ce9be6c7269b7510b67ffde6c41f50fa672ad9
shy <shy@gmail.com>
2025-03-09 11:35:46 +0900

    update plugin ver

--- link-gallery.php
+++ link-gallery.php
@@ -3,7 +3,7 @@
  * Plugin Name: Link Gallery
  * Plugin URI: https://github.com/suhanyujie/wp-link-gallery
  * Description: 一个现代化的友情链接管理插件，使用Blade模板引擎和Illuminate ORM
- * Version: 0.1.1
+ * Version: 0.1.4
  * Author: suhanyujie
  * Author URI: https://github.com/suhanyujie
  * License: GPL v2 or later
@@ -16,7 +16,7 @@ if (!defined('ABSPATH')) {
 }
 
 // 定义插件常量
-define('LINK_GALLERY_VERSION', '0.1.1');
+define('LINK_GALLERY_VERSION', '0.1.4');
 define('LINK_GALLERY_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('LINK_GALLERY_PLUGIN_URL', plugin_dir_url(__FILE__));
 
