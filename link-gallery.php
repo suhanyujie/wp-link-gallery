781452a0df7824221b17cfa3ab6b1f9d1e122634
shy <shy@gmail.com>
2025-03-11 20:58:15 +0900

    数据表的创建和删除调整到 插件的激活和关闭的时候

--- link-gallery.php
+++ link-gallery.php
@@ -25,8 +25,6 @@ require_once LINK_GALLERY_PLUGIN_DIR . 'vendor/autoload.php';
 
 // 初始化插件
 function link_gallery_init() {
-    // 初始化数据库
-    \LinkGallery\Database\Migration::createLinkTable();
     // friend link 后台的菜单和页面显示
     new \LinkGallery\Controllers\Admin\LinkController();
     // 友情链接相关的 gutenberg 编辑器组件
@@ -35,12 +33,19 @@ function link_gallery_init() {
 
 // 激活插件时的处理
 register_activation_hook(__FILE__, function() {
-    // 激活时的处理代码将在这里添加
+    // 初始化数据库
+    \LinkGallery\Database\Migration::createLinkTable();
 });
 
 // 停用插件时的处理
 register_deactivation_hook(__FILE__, function() {
     // 停用时的处理代码将在这里添加
+});
+
+// 卸载插件时删除数据表
+register_uninstall_hook(__FILE__, function() {
+    // 删除数据表
+    \LinkGallery\Database\Migration::dropLinkTable();
 });
 
 // 初始化插件
